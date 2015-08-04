64a4b712d8a6b31eaff4b26bb7cc7946777b6c25
Joe Francis <joe@lostapathy.com>
2015-08-04 17:09:55 -0500

    fully update base box, minimize ruby box output

--- scripts/ubuntu/update.sh
+++ scripts/ubuntu/update.sh
@@ -1,15 +1,10 @@
 #!/bin/bash -eux
 
 apt-get update
-apt-get -y upgrade
+apt-get -y dist-upgrade
 
 # ensure the correct kernel headers are installed
 apt-get -y install linux-headers-$(uname -r)
 
-# update package index on boot
-cat <<EOF > /etc/init/refresh-apt.conf
-description "update package index"
-start on networking
-task
-exec /usr/bin/apt-get update
-EOF
+/sbin/shutdown -r now
+
